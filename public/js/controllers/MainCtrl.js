5fbee0abe4b666bdbe23511597b9c9f2819799af
cecile <jamedina@gmail.com>
2014-05-11 09:48:29 +0000

    layour changes

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -4,7 +4,7 @@
 	angular.module('MainCtrl', []).controller('MainController', function($scope) {
 
 	// add to the scope a tag line
-	$scope.tagline = 'NodeJS - Express - MongoDB - AngularJS - Boostrap : for the win!';	
+	$scope.tagline = '<b>M</b>ongoDB - <b>E</b>xpress - <b>A</b>ngularJS - <b>N</b>odeJS';	
 
 	//TODO: remove this to a different place 
 	$(".btn").tooltip({
